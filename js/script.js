977a311785ce2377db9a214ca87f2280640cc189
M.T Rahmani <mohammadtaharahmani500@gmail.com>
2023-10-07 21:02:03 +0330

    delete last project  and add new project files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function celsius() {
+    let c;
+    c = +document.getElementById("input").value;
+    let f;
+    f = c * 1.8 + 32;
+    document.getElementById("output").value = f;
+}
+alert("Welcom To My Web Site");
